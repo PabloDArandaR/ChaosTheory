b7bb08132e56b49b7654849a3b2eeaa1e94093f5
Pablo <pablo.arandarod@gmail.com>
2024-01-21 20:05:21 +0100

    Sample code used to generate the images.

--- test.py
+++ test.py
@@ -4,24 +4,24 @@ import py_attractor.evaluate as evaluate
 import matplotlib.pyplot as plt
 
 def main():
-    m = cool_attractors.aizawaAttractor(
-        initial_state=[0.1, 0, 0], 
-        dt=0.00001, 
-        a=1, 
-        b=0.7, 
-        c=0.6, 
-        d=3.5, 
-        e=0.25,
-        f=0.1)
+    # m = cool_attractors.aizawaAttractor(
+    #     initial_state=[0.1, 0, 0], 
+    #     dt=0.00001, 
+    #     a=1, 
+    #     b=0.7, 
+    #     c=0.6, 
+    #     d=3.5, 
+    #     e=0.25,
+    #     f=0.1)
     
     m = cool_attractors.arneodoAttractor(
         initial_state=[0.1, 0, 0],
-        dt=0.000003,
+        dt=0.00005,
         a=-5.5,
         b=3.5,
         c=-1.0)
 
-    results = evaluate.run_model(m, 30000000)
+    results = evaluate.run_model(m, 5000000)
     results = evaluate.reduce_solution_size(results, 50)
 
     axs = plt.figure().add_subplot(projection='3d')
